22dfc00dbbb97adf937f8dfeda9fe9808ba984b3
kifou <kifou@192.168.1.25>
2019-02-13 22:18:53 +0100

    commit

--- Twister/src/servlet/friends/DeleteFriend.java
+++ Twister/src/servlet/friends/DeleteFriend.java
@@ -25,7 +25,7 @@ public class DeleteFriend extends HttpServlet {
      */
     public DeleteFriend() {
         super();
-        // TODO Auto-generated constructor stub test pour commit blabla bla
+        // TODO Auto-generated constructor stub
     }
 
 	/**
